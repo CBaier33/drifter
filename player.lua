4d3834a6645a7148d783608da1799231755343f9
Christopher Baier <baier.tech@outlook.com>
2025-06-05 22:42:04 -0400

    firstish commit

--- player.lua
+++ player.lua
@@ -1,10 +1,12 @@
 local Player = {}
 
 function Player:load()
-  self.x = 50
-  self.y = 50
   self.width = 50
   self.height = 50
+
+  self.x = love.graphics.getWidth() / 2 - self.width / 2
+  self.y = love.graphics.getHeight() - 200
+
   self.speed = 500
 
 end
@@ -16,18 +18,28 @@ end
 
 -- player movement methods
 
--- TODO: add rotation to "drift" player and have it return to center when no item is pressed.
 function Player:move(dt)
-  if love.keyboard.isDown("a") and self.x < love.graphics.getWidth()/3  then
-    self.x = self.x + self.speed * dt
-  elseif love.keyboard.isDown("d") and self.x - self.width > -(love.graphics.getWidth() / 3) then
+  if love.keyboard.isDown("a") and not self:atBoundary("l") then
     self.x = self.x - self.speed * dt
+  elseif love.keyboard.isDown("d") and not self:atBoundary("r") then
+    self.x = self.x + self.speed * dt
+  end
+end
+
+function Player:atBoundary(dir)
+  local left = love.graphics.getWidth()/3
+  local right = love.graphics.getWidth()/3*2
+
+  if dir == "l" then
+    return self.x < left
+  elseif dir == "r" then
+    return self.x + self.width > right
   end
 end
 
 function Player:draw()
   -- temporary player sprite
-  love.graphics.rectangle("fill", (love.graphics.getWidth() / 2) - self.x / 2, love.graphics.getHeight() - 200, self.width, self.height)
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 return Player
