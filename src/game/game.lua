7d997f880c52c22f4931f94cf726a8ba4cc3ebcb
JoeBai3r <jo461915@ucf.edu>
2025-07-20 16:20:04 -0400

    refactored obstacles to be obstacle table and simplified logic

--- src/game/game.lua
+++ src/game/game.lua
@@ -3,7 +3,7 @@ Game.__index = Game
 
 -- Dependencies
 local Player = require('game.player')
-local Obstacle = require('game.obstacle')
+local ObstacleTable = require('game.obstacle')
 local Road = require('game.road')
 local Menus = require('menus.menus')
 
@@ -12,18 +12,15 @@ function Game:new(stateManager)
 
   self.stateManager = stateManager
   self.gameActive = true -- temp to avoid crashes after collision
-  self.obstacles = {}
   self.gameTime = 0
 
   Menus:load(stateManager)
   Road:load()
   Player:load()
-
-  local o = Obstacle:new()
-  o:load()
-  table.insert(self.obstacles, o)
+  ObstacleTable:load()
 
   return self
+
 end
 
 function Game:update(dt)
@@ -34,66 +31,29 @@ function Game:update(dt)
 
   if self.gameTime < 1 then return end
 
-  for _, obstacle in ipairs(self.obstacles) do
-    obstacle:update(dt)
+  ObstacleTable:update(dt)
+
+  if self:playerObjectCheckCollision() and self.gameActive then
+    Player:encounterObject()
+    self.gameActive = false
+    self.stateManager:gameOver()
+    -- trigger menus, etc.
   end
 
-  for _, obstacle in ipairs(self.obstacles) do
-    if self:playerObjectCheckCollision(Player, obstacle) and self.gameActive then
-      Player:encounterObject()
-      self.gameActive = false
-      self.stateManager:gameOver()
-      -- trigger menus, etc.
-    end
-  end
-
-  local last = self.obstacles[#self.obstacles]
-  if last and last.obstacle.y > love.graphics.getHeight() / 4 then
-    self:newObstacle()
-  end
-
-  if #self.obstacles > 0 and self.obstacles[1].obstacle.y > love.graphics.getHeight() then
-    table.remove(self.obstacles, 1)
-  end
 end
+
 
 function Game:draw()
   Road:draw()
   Player:draw()
-
-  for _, barrier in ipairs(self.obstacles) do
-    barrier:draw()
-  end
-
+  ObstacleTable:draw()
   Menus:draw()
+
 end
 
-function Game:newObstacle()
-  local newObstacle = Obstacle:new()
-  newObstacle:load()
+function Game:playerObjectCheckCollision()
+  return ObstacleTable:checkCollision(Player)
 
-  while #self.obstacles > 0 and self:obstacleInLane(newObstacle.obstacle.x, newObstacle.obstacle.width) do
-    newObstacle = Obstacle:new()
-    newObstacle:load()
-  end
-
-  table.insert(self.obstacles, newObstacle)
-end
-
-function Game:obstacleInLane(x, width)
-  for _, value in pairs(self.obstacles) do
-    if x + width >= value.obstacle.x and x <= value.obstacle.x + value.obstacle.width then
-      return true
-    end
-  end
-  return false
-end
-
-function Game:playerObjectCheckCollision(player, obstacle)
-  return player.x < obstacle.obstacle.x + obstacle.obstacle.width and
-         obstacle.obstacle.x < player.x + player.width and
-         player.y < obstacle.obstacle.y + obstacle.obstacle.height and
-         obstacle.obstacle.y < player.y + player.height
 end
 
 return Game
--- src/game/obstacle.lua
+++ src/game/obstacle.lua
@@ -5,38 +5,94 @@ local Car = require('game.car')
 local Truck = require('game.truck')
 local TrashPile = require('game.trash')
 
-function Obstacle:new()
-  local self = setmetatable({}, Obstacle)
-  return self
-
-end
-
 function Obstacle:load()
-  self.obstacleType = math.random(3)
-  self.obstacle = nil
-
-  if self.obstacleType == 1 then
-    self.obstacle = Car:new()
-
-  elseif self.obstacleType == 2 then
-    self.obstacle = Truck:new()
-
-  elseif self.obstacleType == 3 then
-    self.obstacle = TrashPile:new()
-
-  end
-
-  self.obstacle:load()
+  self.table = {}
+  self.newObstacle(self)
 
 end
 
 function Obstacle:update(dt)
-    self.obstacle:move(dt)
+  for _, obstacle in ipairs(self.table) do
+    obstacle:move(dt)
   end
 
+  -- adds new obstacle when necessary
+  local last = self.table[#self.table]
+  if last and last.y > love.graphics.getHeight() / 4 then
+    self:newObstacle()
+  end
+
+  -- deletes obstacles that are gone
+  if #self.table > 0 and self.table[1].y > love.graphics.getHeight() then
+    table.remove(self.table, 1)
+  end
+
+end
+
 function Obstacle:draw()
-  self.obstacle:draw()
+  for _, obstacle in ipairs(self.table) do
+    obstacle:draw()
+  end
+
+end
+
+function Obstacle:newObstacle()
+
+  local newObstacle = self:generateObstacle()
+  newObstacle:load()
+
+  while #self.table > 0 and self:obstacleInLane(newObstacle.x, newObstacle.width) do
+    newObstacle = self:generateObstacle()
+    newObstacle:load()
+  end
+
+  table.insert(self.table, newObstacle)
+
+end
+
+function Obstacle:generateObstacle()
+  local obstacleType = math.random(3)
+  local obstacle = nil
+
+  if obstacleType == 1 then
+    obstacle = Car:new()
+
+  elseif obstacleType == 2 then
+    obstacle = Truck:new()
+
+  else
+    obstacle = TrashPile:new()
+
+  end
+
+  return obstacle
+
+end
+
+function Obstacle:obstacleInLane(x, width)
+  for _, value in pairs(self.table) do
+    if x + width >= value.x and x <= value.x + value.width then
+      return true
+    end
+  end
+  return false
+
+end
+
+function Obstacle:checkCollision(player)
+  for _, obstacle in ipairs(self.table) do
+    if player.x < obstacle.x + obstacle.width and
+       obstacle.x < player.x + player.width and
+       player.y < obstacle.y + obstacle.height and
+       obstacle.y < player.y + player.height then
+         return true
+    end
+
+  return false
+
+  end
 
 end
 
 return Obstacle
+
