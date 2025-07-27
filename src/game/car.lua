d1e106e98bee6ddb14c21a5f2bbb5316cf0e4416
JoeBai3r <jo461915@ucf.edu>
2025-07-27 18:51:59 -0400

    obstacle directions reversed and speeds adjusted

--- src/game/car.lua
+++ src/game/car.lua
@@ -7,7 +7,7 @@ function Car:new()
   self.width = 50
   self.height = 80
 
-  self.speed = 1300
+  self.speed = 300
   self.mobile = true
 
   return self
--- src/game/truck.lua
+++ src/game/truck.lua
@@ -7,7 +7,7 @@ function Truck:new()
   self.width = 50
   self.height = 95
 
-  self.speed = 1200
+  self.speed = 500
   self.mobile = true
 
   return self
